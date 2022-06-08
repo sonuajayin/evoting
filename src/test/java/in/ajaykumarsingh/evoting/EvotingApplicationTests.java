327c9b743cb77095daec62bc8a048d5523830206
Ajay Singh <you@example.com>
2022-06-08 23:22:47 +0530

    Created Project

---
+++ src/test/java/in/ajaykumarsingh/evoting/EvotingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ajaykumarsingh.evoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
